c14407e3fddaca30f295bba6b5d7b37cc5e9744c
Pavel Stebunov <pavel.stebunov@gmail.com>
2024-04-22 14:47:38 +0300

    done!

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,49 @@
+package web.model;
+
+public class Car {
+    String brand;
+    String model;
+    String color;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public Car(String brand, String model, String color) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+
+    public Car() {
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
